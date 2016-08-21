225eccbcae7c589e154941bdc27fa64a9018521a
defrag257 <defrag257@qq.com>
2016-08-21 20:37:53 +0800

    jniklemgfdsmglkef

---
+++ setenv.sh
@@ -0,0 +1,7 @@
+echo edit this file and uncomment the next line to put your password
+#echo https://defrag257:???@github.com>$HOME/.git-credentials
+git config --global credential.helper store
+git config --global gui.encoding utf-8
+git config --global i18n.commitencoding gbk
+git config --global svn.pathnameencoding gbk
+
