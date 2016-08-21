5f84dc2c61791070b7d1c813dd8b8ea2e8c00786
defrag257 <defrag257@qq.com>
2016-08-21 21:03:10 +0800

    sanfkjlmdsafkmew

--- readme.txt
+++ readme.txt
@@ -11,3 +11,5 @@ https://tortoisegit.org/download/
 
 空白项目第一次push要选上强制“未知变更”，不然会发生错误。
 
+使用./setenv.sh password可以设置密码。
+
--- setenv.sh
+++ setenv.sh
@@ -1,3 +1,6 @@
-echo edit this file and uncomment the next line to put your password
-#echo https://defrag257:???@github.com>$HOME/.git-credentials
-git config --global credential.helper store
+if [ "$1" == "" ]; then
+	echo usage: ./setenv.sh password
+else
+	echo https://defrag257:$1@github.com>$HOME/.git-credentials
+	git config --global credential.helper store
+fi
