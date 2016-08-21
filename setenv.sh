c62cb160aaba36307c3494c7678d568314e378f3
defrag257 <defrag257@qq.com>
2016-08-21 20:53:07 +0800

    dsgnflksf

--- setenv.sh
+++ setenv.sh
@@ -3,5 +3,5 @@ echo edit this file and uncomment the next line to put your password
 git config --global credential.helper store
 git config --global gui.encoding utf-8
 git config --global i18n.commitencoding gbk
-git config --global svn.pathnameencoding gbk
+git config --global svn.pathnameencoding utf-8
 
